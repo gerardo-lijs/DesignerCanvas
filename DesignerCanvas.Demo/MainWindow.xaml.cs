016c7c0e4acbf672e3f2773e748b725e64661505
Gerardo Lijs <gerardo@micropticsl.com>
2021-04-05 07:49:23 +0200

    move styles inside custom control

--- DesignerCanvas.Demo/MainWindow.xaml.cs
+++ DesignerCanvas.Demo/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace DesignerCanvasDemo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
