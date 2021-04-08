7099848825b06e8c1dbf3425ac9823b68edca0f9
Gerardo Lijs <gerardo@micropticsl.com>
2021-04-08 17:19:03 +0200

    add label adorner

---
+++ DesignerCanvas/Controls/LabelAdorner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace DesignerCanvas.Controls
+{
+    public class LabelAdorner : Adorner
+    {
+        private readonly Brush _backgroundBrush;
+        private readonly string _label;
+
+        public LabelAdorner(UIElement adornedElement, Brush backgroundBrush, string label) : base(adornedElement)
+        {
+            _backgroundBrush = backgroundBrush;
+            _label = label;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            var formattedText = new FormattedText(_label, CultureInfo.InvariantCulture, FlowDirection.LeftToRight, new Typeface(new FontFamily("Segoe UI"), FontStyles.Normal, FontWeights.Bold, FontStretches.Normal), 12, Brushes.White);
+            drawingContext.DrawRectangle(_backgroundBrush, null, new Rect(0, -21, formattedText.Width + 10, 20));
+            drawingContext.DrawText(formattedText, new Point(4, -19));
+        }
+    }
+}
